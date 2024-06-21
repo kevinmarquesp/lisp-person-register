677b6f723459b74d7c87c6d9acc8ae22ce4020dd
Kevin Marques <kevinmarqueskmp@gmail.com>
2024-06-21 20:27:08 -0300

    :construction: wip: Connecting to the database into the tmp/ directory and inserting some data

--- src/main.lisp
+++ src/main.lisp
@@ -2,6 +2,22 @@
   (:use :cl))
 (in-package :lisp-person-register)
 
+;; Constant work around from: https://stackoverflow.com/questions/34800988/sbcl-asdfload-system-fails-when-a-string-constant-is-defined
+
+(eval-when (:compile-toplevel)
+  (defconstant +database+ "tmp/database.sqlite3"
+    "Path to the SQLite3 database file, where the persons will be registered."))
+
+(mito:deftable user ()
+  ((name :col-type (:varchar 128))
+   (age :col-type :integer)))
+
+(mito:connect-toplevel :sqlite3 :database-name +database+)
+
+(mito:ensure-table-exists 'user)
+
+(mito:insert-dao
+  (make-instance 'user :name "Kevin Marques" :age 19))
+
 (defun start ()
-  "Will simply display a hello world message on the screen."
-  (format t "Hello world! (updated)~%"))
+  (format t "End.~%"))
